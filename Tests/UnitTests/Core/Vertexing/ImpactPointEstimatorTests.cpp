74e075fcd0ffc48cb334cc12c880f7f7aac4f965
Bastian Schlag <bastian.schlag@cern.ch>
2019-06-10 12:29:13 +0200

    store vertex pointer in state.collectoin to be able to consistently use mapping to InfoMap, adapt all other tools/methods accordingly

--- Tests/UnitTests/Core/Vertexing/ImpactPointEstimatorTests.cpp
+++ Tests/UnitTests/Core/Vertexing/ImpactPointEstimatorTests.cpp
@@ -27,7 +27,6 @@ namespace Test {
 
 using Covariance = BoundSymMatrix;
 using Propagator = Propagator<EigenStepper<ConstantBField>>;
-
 // Create a test context
 GeometryContext geoContext = GeometryContext();
 MagneticFieldContext magFieldContext = MagneticFieldContext();
