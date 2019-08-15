0e0bd32f3c5d044205902fcfbcd786a624571f65
Fabian Klimpel <fklimpel@cern.ch>
2019-08-15 15:58:37 +0200

    Compiler complains moved to unit tests

--- Tests/UnitTests/Core/Fitter/KalmanFitterTests.cpp
+++ Tests/UnitTests/Core/Fitter/KalmanFitterTests.cpp
@@ -48,7 +48,6 @@ namespace Test {
 
 // A few initialisations and definitionas
 using SourceLink = MinimalSourceLink;
-using Jacobian = BoundMatrix;
 using Covariance = BoundSymMatrix;
 
 using Resolution = std::pair<ParID_t, double>;
@@ -193,7 +192,7 @@ struct MaterialScattering {
     // Check if there is a surface with material and a covariance is existing
     if (state.navigation.currentSurface &&
         state.navigation.currentSurface->surfaceMaterial() &&
-        state.stepping.cov != Covariance::Zero()) {
+        state.stepping.cov != typename stepper_t::Covariance::Zero()) {
       // Sample angles
       std::normal_distribution<double> scatterAngle(
           0., 0.017);  //< \approx 1 degree
