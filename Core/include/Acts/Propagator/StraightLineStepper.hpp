ce1aef5d09bdda5ff8fe0a3b244ff1fbd984b780
Fabian Klimpel <fklimpel@cern.ch>
2019-10-29 10:20:36 +0100

    Decomposition of SLS cov into smaller functions

--- Core/include/Acts/Propagator/StraightLineStepper.hpp
+++ Core/include/Acts/Propagator/StraightLineStepper.hpp
@@ -35,9 +35,9 @@ namespace Acts {
 class StraightLineStepper {
  public:
   using Jacobian = BoundMatrix;
-  using Covariance = FreeSymMatrix;
-  using BoundState = std::tuple<BoundParameters, BoundMatrix, double>;
-  using CurvilinearState = std::tuple<CurvilinearParameters, BoundMatrix, double>;
+  using Covariance = std::variant<BoundSymMatrix, FreeSymMatrix>;
+  using BoundState = std::tuple<BoundParameters, Jacobian, double>;
+  using CurvilinearState = std::tuple<CurvilinearParameters, Jacobian, double>;
   using BField = NullBField;
 
   /// State for track parameter propagation
